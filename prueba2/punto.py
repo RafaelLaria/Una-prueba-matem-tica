01b14084374d1fc4d9b10ed226d1869d1ecc1149
RafaelLaria <rafael.lariaortiz@usp.ceu.es>
2025-10-24 11:57:01 +0200

    primer cambio

---
+++ prueba2/punto.py
@@ -0,0 +1,22 @@
+class Punto:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def __str__(self):
+        return print(f"{x},{y}")
+
+    def punto.cuadrante(self):
+        if x > 0 and y > 0:
+            print("El punto pertenece al primer cuadrante")
+        elif x < 0 and y > 0:
+            print("El punto pertenece al segundo cuadrante")
+        elif x < 0 and y < 0:
+            print("El punto pertenece al tercer cuadrante")
+        elif x > 0 and y < 0:
+            print("El punto pertenece al cuarto cuadrante")
+        else:
+            print("El punto está en el origen")
+        return True
+    import math
+    def punto.distancia(self)
