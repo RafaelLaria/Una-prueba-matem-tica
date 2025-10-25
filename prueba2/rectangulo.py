635a3634c8003ebb003cc87f1140fd6dcc00d1cc
RafaelLaria <rafael.lariaortiz@usp.ceu.es>
2025-10-25 12:25:00 +0200

    ejercicio resuelto

--- prueba2/rectangulo.py
+++ prueba2/rectangulo.py
@@ -8,9 +8,12 @@ class Rectangulo:
             p2 = Punto()
         self.p1 = p1
         self.p2 = p2
+
     def base(self):
         return abs(self.p2.x - self.p1.x)
+    
     def altura(self):
         return abs(self.p2.y - self.p1.y)
+    
     def area(self):
         return self.base() * self.altura() 
